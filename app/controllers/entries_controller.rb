c000e7bc2c50bdba10671ee78940738d82abee60
Damian Hallengren <djhallengren@gmail.com>
2023-12-05 15:50:44 +0100

    created logic for diary entries being created, authentication of users

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,18 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    today = Entry.find { |entry| entry.date == Date.today }
+    if today
+      p "Loading today's data"
+      @entry = today
+    else
+      p "Creating new entry"
+      @entry = Entry.new
+      @entry.date = Date.today
+      @entry.user = current_user
+      p @entry.date
+      @entry.save
+    end
   end
 end
---
+++ app/javascript/components/Diary.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Diary = (diary) => {
+  return (
+    <React.Fragment>
+      {diary}
+    </React.Fragment>
+  )
+}
+
+
+export default Diary
