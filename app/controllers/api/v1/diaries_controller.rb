94c83eaf7fd6eecf51a25170cf40c2cc4d279c73
Damian Hallengren <djhallengren@gmail.com>
2023-12-14 16:01:26 +0100

    Internal requests via JS working(?)

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::DiariesController < ApplicationController
+  before_action :authenticate_user!
   def show
     @diary = Diary.find(params[:id])
-    render json: @diary, status: :ok
+    if @diary.user == current_user
+      render json: @diary, status: :ok
+    else
+      render json: {error: "Unauthorized access attempt"}, status: :forbidden
+    end
   end
 end
--- app/javascript/components/Diary.js
+++ app/javascript/components/Diary.js
@@ -1,14 +1,28 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { useState } from 'react'
+import { useEffect } from 'react';
 
-const Diary = (props) => {
-  const diary = {
-    content: "Today, I saw a fish in the pond.  It was very, very meaningful.",
-    mood: "Happy"
+const Diary = ({id}) => {
+  const url = `/api/v1/diaries/${id}`
+  const diary = grabData(url)
+  // const content = diary.content
+  // const mood = diary.mood
+
+  function grabData(url) {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+      }
   }
+  console.log(diary)
+  console.log(diary.id)
+
   return (
     <React.Fragment>
       <div className="diary-block">
+        <p>{diary.id}</p>
         <form>
           <textarea>
           </textarea>
