607812d2c44f8fa69f98d527327a40ebab8f7243
Damian Hallengren <djhallengren@gmail.com>
2023-12-17 23:25:10 +0100

    fixed my wonky callsby using useeffect properly

--- app/javascript/components/Diary.js
+++ app/javascript/components/Diary.js
@@ -4,31 +4,28 @@ import { useEffect } from 'react';
 
 const Diary = ({id}) => {
   const url = `/api/v1/diaries/${id}`
-  let diary
-
+  const [diary, setDiary] = useState([])
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((body) => {
-        console.log(body)
-        diary = body
+        setDiary(body)
       })
-  }, [diary, url])
-
+  }, [])
   console.log(diary)
-
-  return (
-    <React.Fragment>
-      <div className="diary-block">
-        <p></p>
-        <form>
-          <textarea>
-          </textarea>
-          <button type="submit">Save</button>
-        </form>
-      </div>
-    </React.Fragment>
-  )
+  if (!!diary.id) {
+    return (
+      <React.Fragment>
+        <div className="diary-block">
+          <form>
+            <textarea>
+            </textarea>
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 
