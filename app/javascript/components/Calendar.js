8598d7bf08021b9ec8ed1939b3b590702f80d204
Damian Hallengren <djhallengren@gmail.com>
2024-01-08 13:53:51 +0100

    Calendar now displays correct days with offset for day of week

--- app/javascript/components/Calendar.js
+++ app/javascript/components/Calendar.js
@@ -4,14 +4,36 @@ const Calendar = (props) => {
   // Will need an event model in the rails portion of the app?
 
   // Create a grid?  Just rely on snaking behavior with flex?
-  let startDate
-  let endDate
 
   const today = new Date
-  console.log(today + 1)
+
   const month = today.getMonth()
   const year = today.getFullYear()
 
+  console.log(today.getDay())
+
+
+  const startDate = new Date(year, month, 1)
+  const endDate = new Date(year, month + 1, 0)
+
+  let days = []
+
+  const offset = startDate.getDay()
+
+  for (let i = 0; i < offset; i++) {
+    days.push(<h3></h3>)
+  }
+
+  let active = startDate
+  while (active <= endDate) {
+    days.push(active.getDate())
+    active.setDate(active.getDate() + 1)
+  }
+
+  console.log(days)
+  const dayList = days.map(day =>
+    <h3 key={`${day}/${month}/${year}`}>{day}</h3>
+  )
 
 
   return (
@@ -21,7 +43,14 @@ const Calendar = (props) => {
           <h2>Calendar</h2>
         </div>
         <div className="calendar-body">
-
+          <h3>Sun</h3>
+          <h3>Mon</h3>
+          <h3>Tue</h3>
+          <h3>Wed</h3>
+          <h3>Thu</h3>
+          <h3>Fri</h3>
+          <h3>Sat</h3>
+          {dayList}
         </div>
       </div>
     </React.Fragment>
